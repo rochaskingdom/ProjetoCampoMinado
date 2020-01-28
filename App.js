83990acb12656d2443d464136f7b6cafeca52ca2
Vinicius Rocha <viniciusr.rocha@outlook.com>
2020-01-28 13:04:15 -0300

    Componente Tabuleiro (MineField)

--- App.js
+++ App.js
@@ -6,26 +6,40 @@ import {
 } from 'react-native';
 
 import params from './src/params';
-import Field from './src/components/Field'
+import MineField from './src/components/MineField'
+import {
+  createMinedBoard
+} from './src/functions'
 export default class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = this.createState()
+  }
+
+	minesAmount = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+    return Math.ceil(cols * rows * params.difficultLevel)
+  }
+
+  createState = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+    return {
+      board: createMinedBoard(rows, cols, this.minesAmount()),
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text>Iniciando o Mines!</Text>
         <Text>Tamanho da grade:
           {params.getRowsAmount()}x{params.getColumnsAmount()}</Text>
-
-          <Field />
-          <Field opened />
-          <Field opened nearMines={1} />
-          <Field opened nearMines={2} />
-          <Field opened nearMines={3} />
-          <Field opened nearMines={6} />
-          <Field mined />
-          <Field mined opened/>
-          <Field mined opened exploded/>
-          <Field flagged />
-          <Field flagged opened />
+        <View style={styles.board}>
+            <MineField board={this.state.board} />
+        </View>
       </View>
     )
   }
@@ -34,8 +48,10 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
+  },
+  board: {
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#AAA',
   },
 });
